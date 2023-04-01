d04f152516cbcc16274511de8c46baf1ba0c9bcb
Edsandro Freitas <edsandrofreitas@gmail.com>
2023-03-31 23:25:28 -0300

    feat: create spring boot project

---
+++ src/main/java/com/github/edsandrof/roomreservation/RoomReservationApplication.java
@@ -0,0 +1,13 @@
+package com.github.edsandrof.roomreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/edsandrof/roomreservation/RoomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.edsandrof.roomreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
